d7465b659de2baf6c57999b52538ece7551ddf4a
Fred <f.fotre@gmail.com>
2023-09-26 10:28:01 +0200

    Update ShuflerExtension.php
    TwigExtension adding is_safe html

--- src/Twig/Extension/ShuflerExtension.php
+++ src/Twig/Extension/ShuflerExtension.php
@@ -15,7 +15,7 @@ class ShuflerExtension extends AbstractExtension
             new TwigFilter('categorieDisplay', [ShuflerRuntime::class, 'categoryFilter']),
             new TwigFilter('genreDisplay', [ShuflerRuntime::class, 'genreFilter']),
             new TwigFilter('yearDisplay', [ShuflerRuntime::class, 'yearFilter']),
-            new TwigFilter('convertFrame', [ShuflerRuntime::class, 'convertFrameFilter']),
+            new TwigFilter('convertFrame', [ShuflerRuntime::class, 'convertFrameFilter'], ['is_safe' => ['html']),
             new TwigFilter('youtubeChannelId', [ShuflerRuntime::class, 'getYoutubeChannelId']),
             new TwigFilter('popUp', [ShuflerRuntime::class, 'popUpFilter']),
             new TwigFilter('toIconAlert', [ShuflerRuntime::class, 'toIconAlertFilter'])
@@ -28,7 +28,7 @@ class ShuflerExtension extends AbstractExtension
             new TwigFunction('categorieDisplay', [ShuflerRuntime::class, 'categoryFilter']),
             new TwigFunction('genreDisplay', [ShuflerRuntime::class, 'genreFilter']),
             new TwigFunction('yearDisplay', [ShuflerRuntime::class, 'yearFilter']),
-            new TwigFunction('convertFrame', [ShuflerRuntime::class, 'convertFrameFilter']),
+            new TwigFunction('convertFrame', [ShuflerRuntime::class, 'convertFrameFilter'], ['is_safe' => ['html']),
             new TwigFunction('youtubeChannelId', [ShuflerRuntime::class, 'getYoutubeChannelId']),
             new TwigFunction('popUp', [ShuflerRuntime::class, 'popUpFilter']),
             new TwigFunction('toIconAlert', [ShuflerRuntime::class, 'toIconAlertFilter'])
