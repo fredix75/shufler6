912d81d4ae7853e5ca017a570384f7ce38d108f2
fredix75 <fred_ric@hotmail.com>
2023-10-28 14:04:50 +0200

    bascule modal-form to main_controller

--- assets/controllers/main_controller.js
+++ assets/controllers/main_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
 import $ from 'jquery';
 import TomSelect from "tom-select";
+import {Modal} from "bootstrap";
 
 export default class extends Controller {
+    static values = {
+        formUrl: String,
+    }
+    static targets = ['modal', 'modalBody'];
 
+    async openModal(event) {
+        const modal = new Modal('#formModal', {keyboard: false});
+        modal.show();
+        $(document).find('.modal-body').html(await $.ajax(this.formUrlValue));
+    }
+    
     toggleSlide(event) {
         $('#slider').slideToggle();
     }
