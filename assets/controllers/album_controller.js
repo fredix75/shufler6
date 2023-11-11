eacfe0ff0a51eee9791d75fc4d7f2d9851737371
fredix75 <fred_ric@hotmail.com>
2023-11-11 17:47:57 +0100

    Fix somes

--- assets/controllers/album_controller.js
+++ assets/controllers/album_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
         let artist = $(event.target).closest('a').data('artist');
         artist = artist.replaceAll('&', '%26');
         let album = $(event.target).closest('a').data('album');
-        album = album.replaceAll('&', '%26');
+        album = album.toString().replaceAll('&', '%26');
         let url = '/fr/music/tracks_album';
         let query = '?artist=' + artist + '&album=' + album;
         let content = await $.ajax(url + query);
