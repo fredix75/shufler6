72e24864c2a0b19ba369b6b6ad8e0e2faf132421
fredix75 <fred_ric@hotmail.com>
2024-03-09 19:41:34 +0100

    Cleaning

--- script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then(function () {console.log('Enregistrement reussi.')})
-        .catch(function (e) {console.error(e)});
-}
