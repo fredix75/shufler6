384bdf6436410df9d2cc7966dd0267b864503ea0
fredix75 <fred_ric@hotmail.com>
2024-03-09 20:54:37 +0100

    Cleaning Code

--- src/Form/ChannelFluxFormType.php
+++ src/Form/ChannelFluxFormType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\ChannelFlux;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
