16a73c6ce7f6287941abb91f52e152899cb3e26b
ffotre <f.fotre@gmail.com>
2024-07-19 12:03:33 +0200

    fix playlists

--- assets/controllers/video_controller.js
+++ assets/controllers/video_controller.js
@@ -5,33 +5,35 @@ import $ from 'jquery';
 
 export default class extends Controller {
     connect() {
-        $('.video-link').magnificPopup({
-            type: 'iframe',
-            iframe: {
-                patterns: {
-                    dailymotion: {
-                        index: 'dailymotion.com',
-                        id: function (url) {
-                            var m = url.match(/^.+dailymotion.com\/(embed\/video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
-                            if (m !== null) {
-                                console.log(m);
-                                if (m[4] !== undefined) {
-                                    return m[4];
+        if ($('.video-link').length > 0) {
+            $('.video-link').magnificPopup({
+                type: 'iframe',
+                iframe: {
+                    patterns: {
+                        dailymotion: {
+                            index: 'dailymotion.com',
+                            id: function (url) {
+                                var m = url.match(/^.+dailymotion.com\/(embed\/video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+                                if (m !== null) {
+                                    console.log(m);
+                                    if (m[4] !== undefined) {
+                                        return m[4];
+                                    }
+                                    return m[2];
                                 }
-                                return m[2];
-                            }
-                            return null;
+                                return null;
+                            },
+                            src: 'https://www.dailymotion.com/embed/video/%id%'
                         },
-                        src: 'https://www.dailymotion.com/embed/video/%id%'
-                    },
-                    youtube: {
-                        index: 'youtube.com',
-                        id: 'v=',
-                        src: '//www.youtube.com/embed/%id%?autoplay=1&iv_load_policy=3',
+                        youtube: {
+                            index: 'youtube.com',
+                            id: 'v=',
+                            src: '//www.youtube.com/embed/%id%?autoplay=1&iv_load_policy=3',
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
 
         $(document).magnificPopup({
             delegate: '.playlist-link',
