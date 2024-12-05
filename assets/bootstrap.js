1e1eaba759217a6379eeccb834ffb68cdfed22cb
fredix75 <fred_ric@hotmail.com>
2024-12-05 14:58:41 +0100

    recipes update

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,8 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+const app = startStimulusApp();
 import Autocomplete from 'stimulus-autocomplete'
 
-
-const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 
