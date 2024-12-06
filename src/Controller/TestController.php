7c95e89c21f09f3ed2b580ccf65c7f76b24437ff
fredix75 <fred_ric@hotmail.com>
2024-12-06 17:24:17 +0100

    recipes update

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -16,7 +16,7 @@ class TestController extends AbstractController
 
         $process = new Process(['python3',
             '../bin/dominant_color_finder.py',
-           'https://st4.depositphotos.com/10484988/37877/i/450/depositphotos_378770748-stock-photo-starry-sky-blue-sky-together.jpg',
+           'https://img.youtube.com/vi/VQh8oh0rj3s/0.jpg',
             // 'https://www.thoughtco.com/thmb/OVVzRivlUr6QFRi9fVabr0blZ-k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chimpanzee---pan-troglodytes-troglodytes--831042278-5a5e4c81b39d03003785777f.jpg'
         ]);
         $process->run();
