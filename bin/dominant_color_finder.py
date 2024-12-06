a339a6239df30812a5d3c84cd6072035b4e2df56
fredix75 <fred_ric@hotmail.com>
2024-12-06 17:09:53 +0100

    recipes update

--- bin/dominant_color_finder.py
+++ bin/dominant_color_finder.py
@@ -12,27 +12,27 @@ import numpy
 from PIL import Image
 
 
-    image_path = sys.argv[1]
-    response = requests.get(image_path)
-    image = Image.open(BytesIO(response.content))
-    image = image.resize((150, 150))      # optional, to reduce time
-    ar = numpy.asarray(image)
-    shape = ar.shape
-    ar = ar.reshape(numpy.product(shape[:2]), shape[2]).astype(float)
+image_path = sys.argv[1]
+response = requests.get(image_path)
+image = Image.open(BytesIO(response.content))
+image = image.resize((150, 150))      # optional, to reduce time
+ar = numpy.asarray(image)
+shape = ar.shape
+ar = ar.reshape(numpy.product(shape[:2]), shape[2]).astype(float)
 
-    kmeans = sklearn.cluster.MiniBatchKMeans(
-        n_clusters=10,
-        init="k-means++",
-        max_iter=20,
-        random_state=1000
-    ).fit(ar)
-    codes = kmeans.cluster_centers_
+kmeans = sklearn.cluster.MiniBatchKMeans(
+    n_clusters=10,
+    init="k-means++",
+    max_iter=20,
+    random_state=1000
+).fit(ar)
+codes = kmeans.cluster_centers_
 
-    vecs, _dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
-    counts, _bins = numpy.histogram(vecs, len(codes))    # count occurrences
+vecs, _dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+counts, _bins = numpy.histogram(vecs, len(codes))    # count occurrences
 
-    colors = []
-    for index in numpy.argsort(counts)[::-1]:
-        colors.append(tuple([int(code) for code in codes[index]]))
+colors = []
+for index in numpy.argsort(counts)[::-1]:
+    colors.append(tuple([int(code) for code in codes[index]]))
 
-    print(colors)
+print(colors)
