1c04b6f07d7a8b55a8b1842060a2451701cb3c58
fredix75 <fred_ric@hotmail.com>
2024-12-06 17:19:22 +0100

    recipes update

--- bin/dominant_color_finder.py
+++ bin/dominant_color_finder.py
@@ -5,35 +5,26 @@ import struct
 from PIL import Image
 import os
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
-from io import BytesIO
+
+import numpy as np
+import scipy
+import scipy.misc
 import scipy.cluster
-import sklearn.cluster
-import numpy
-from PIL import Image
-import binascii
+from io import BytesIO
+
+NUM_CLUSTERS = 5
 
 image_path = sys.argv[1]
 response = requests.get(image_path)
-image = Image.open(BytesIO(response.content))
-image = image.resize((150, 150))      # optional, to reduce time
-ar = numpy.asarray(image)
+img = Image.open(BytesIO(response.content))
+im = img.resize((150, 150))      # optional, to reduce time
+ar = np.asarray(im)
 shape = ar.shape
-ar = ar.reshape(numpy.prod(shape[:2]), shape[2]).astype(float)
-
-kmeans = sklearn.cluster.MiniBatchKMeans(
-    n_clusters=10,
-    init="k-means++",
-    max_iter=20,
-    random_state=1000
-).fit(ar)
-codes = kmeans.cluster_centers_
-
-vecs, _dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
-counts, _bins = numpy.histogram(vecs, len(codes))    # count occurrences
-
-colors = []
-for index in numpy.argsort(counts)[::-1]:
-    colors.append(tuple([int(code) for code in codes[index]]))
-
-colour = binascii.hexlify(bytearray(int(c) for c in colors)).decode('ascii')
+ar = ar.reshape(scipy.prod(shape[:2]), shape[2]).astype(float)
+codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+index_max = scipy.argmax(counts)                    # find most frequent
+peak = codes[index_max]
+colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
 print('#%s' % colour)
