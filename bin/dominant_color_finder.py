9ad6e601ddd450fefa1037f760b96b71c47b1134
fredix75 <fred_ric@hotmail.com>
2024-12-06 17:01:10 +0100

    recipes update

--- bin/dominant_color_finder.py
+++ bin/dominant_color_finder.py
@@ -8,7 +8,7 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 import numpy as np
 import scipy
-from scipy import integrate
+from scipy import stats
 import scipy.misc
 import scipy.cluster
 from io import BytesIO
@@ -21,7 +21,7 @@ img = Image.open(BytesIO(response.content))
 im = img.resize((150, 150))      # optional, to reduce time
 ar = np.asarray(im)
 shape = ar.shape
-ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
+ar = ar.reshape(stats.product(shape[:2]), shape[2]).astype(float)
 codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
 vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
 counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
