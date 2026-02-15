60c0042e55a005f059968433c71482710d6af872
redfox <fred@shufler.fr>
2026-02-15 11:43:15 +0100

    clean

--- src/Repository/MusicCollection/AlbumRepository.php
+++ src/Repository/MusicCollection/AlbumRepository.php
@@ -106,29 +106,4 @@ class AlbumRepository extends ServiceEntityRepository
 
         return $paginator;
     }
-
-//    /**
-//     * @return Album[] Returns an array of Album objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Album
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
