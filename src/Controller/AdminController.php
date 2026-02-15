66d559062324c765ef75b6f7471bdbebc21b217d
redfox <fred@shufler.fr>
2026-02-15 17:27:14 +0100

    ExtraNotation Reseter

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Form\ExtraNotationType;
 use App\Helper\ApiRequester;
 use App\Helper\VideoHelper;
 use App\Repository\MusicCollection\TrackRepository;
 use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 #[Route('/admin', name: 'admin')]
 #[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     #[Route('/dead_videos', name: '_dead_videos')]
     public function deadVideos(VideoRepository $videoRepository, ApiRequester $apiRequester, VideoHelper $videoHelper): Response
     {
@@ -56,14 +68,32 @@ class AdminController extends AbstractController
     }
 
     #[Route('/extra-note', name: '_extra_note')]
-    public function listExtraNote(TrackRepository $trackRepository): Response
+    public function listExtraNote(Request $request, TrackRepository $trackRepository): Response
     {
         $tracks = $trackRepository->createQueryBuilder('t')
             ->where('t.extraNote IS NOT NULL')
             ->getQuery()
             ->getResult();
 
+        $form = $this->createForm(ExtraNotationType::class, null, [
+            'tracks' => $tracks,
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $tracks = $form->get('extra_graduated_tracks')->getData();
+            foreach ($tracks as $track) {
+                $track->setNote(4);
+                $track->setExtraNote(null);
+            }
+            $trackRepository->save($track, true);
+
+            $this->addFlash('success', 'La note de ces titres a été mise à jour');
+            return $this->redirectToRoute('admin_extra_note');
+        }
+
         return $this->render('admin/extra_notes.html.twig', [
+            'form' => $form,
             'tracks' => $tracks,
         ]);
     }
---
+++ src/Form/ExtraNotationType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MusicCollection\Track;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExtraNotationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('extra_graduated_tracks', ChoiceType::class, [
+                'choices' => $options['tracks'],
+                'choice_value' => fn(Track $track) => $track->getId(),
+                'choice_label' => fn(Track $track) => sprintf("%s - %s - %s", $track->getTitre(), $track->getAuteur(), $track->getAlbum()),
+                'multiple' => true,
+                'expanded' => true,
+                'row_attr'=> [
+                    'class' => 'form-check form-switch'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'tracks' => []
+        ]);
+    }
+}
