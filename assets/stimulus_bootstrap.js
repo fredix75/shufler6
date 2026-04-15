df041cd19577a7b0ad86388d35c15f19eb3eb3af
ffotre <f.fotre@gmail.com>
2026-04-15 15:56:40 +0200

    composer update

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,6 +1,9 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
 const app = startStimulusApp();
+import { startStimulusApp } from '@symfony/stimulus-bundle';
+
+const app = startStimulusApp();
 
 import Autocomplete from 'stimulus-autocomplete'
 
